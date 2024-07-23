b27c9868f5eb5ec5cf302904fece40b6f466e36d
kaushal <kaushalkumar02918@gmail.com>
2024-07-24 04:13:48 +0530

    fix some thing

--- shery.js
+++ shery.js
@@ -40,6 +40,7 @@ Shery.imageEffect(".sec-img4", {
 
 
 
+
 Shery.textAnimate(".sectionLeft h1" , {
   //Parameters are optional.
   style: 1,
@@ -50,14 +51,4 @@ Shery.textAnimate(".sectionLeft h1" , {
   multiplier: 0.1,
 });
 
-document.querySelector(".bur-text-box").addEventListener("mousemove",(e)=>{
-  Shery.textAnimate(".good-text" , {
-    //Parameters are optional.
-    style: 2,
-    y: 10,
-    delay: 0.1,
-    duration: 2,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    multiplier: 0.1,
-  });
-})
+
